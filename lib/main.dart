ac76bc6443db5051aa93d02f497121f93ea1cae8
American Chauhan <42908640+americanchauhan@users.noreply.github.com>
2022-05-21 22:04:23 +0530

    Day 3completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:first_flutter/home_page.dart';
+import 'package:first_flutter/pages/home_page.dart';
+import 'package:first_flutter/pages/login_page.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,13 +8,21 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: HomePage(),
+      //home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
